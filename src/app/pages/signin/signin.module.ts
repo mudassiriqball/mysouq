a7014457a83fdd648d8c36d999c544e0dc416f15
mudassiriqball <mudassir.iqball@hotmail.com>
2019-11-21 13:19:50 +0500

    first commint mysouq

---
+++ src/app/pages/signin/signin.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SigninPageRoutingModule } from './signin-routing.module';
+
+import { SigninPage } from './signin.page';
+import  '../../components/social-buttons/social-buttons.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SigninPageRoutingModule,
+  ],
+  declarations: [SigninPage]
+})
+export class SigninPageModule {}
---
+++ src/app/pages/signin/signin.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import '../../components/social-buttons';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.page.html',
+  styleUrls: ['./signin.page.scss'],
+})
+export class SigninPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
